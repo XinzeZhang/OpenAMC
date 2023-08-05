8836474cfbaf8a8bf762c5191691854abfefb7bd
xinzezhang <xinze.zh@outlook.com>
2023-08-05 23:52:27 +0800

    update

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     args = get_parser()
     args.cuda = True
     
-    args.expdatafolder = 'exp_config/rml16a'
-    args.exp_file= 'demo'
+    args.expdatafolder = 'exp_config/xinze'
+    args.exp_file= 'rml16a'
     args.exp_name = 'paper.test'
     
     args.test = True
